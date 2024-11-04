95a31a7fb051c0461005cccc71043290d37d1ae0
Neraky <felipecartaxo1@gmail.com>
2024-11-04 17:22:10 -0300

    getUser(id) request - adding annotation

--- src/main/java/com/cartaxo/users_crud/resource/UserResource.java
+++ src/main/java/com/cartaxo/users_crud/resource/UserResource.java
@@ -3,10 +3,7 @@ package com.cartaxo.users_crud.resource;
 import com.cartaxo.users_crud.entities.User;
 import com.cartaxo.users_crud.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,13 +21,15 @@ public class UserResource {
         return service.findAll();
     }
 
+    // Retorna um usuário a partir do id passado na URL
     @GetMapping("/{id}") // Responde a requisições GET feitas para /api/users/{id}. Serve para obter os detalhes de um usuário a partir de um id
     public User getUser(@PathVariable Long id) { // O id do usuário é extraído da URL e passado como argumento
         return service.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
     }
 
-
-
-
-
+    @PostMapping // Este método será chamado quando uma requisição HTTP do tipo POST for feita para /api/users. É utilizado para criar um novo usuário
+    // Cria um usuário
+    public User createUser(@RequestBody User user) { // Determina que o corpo da requisição será transformado em uma instância da classe User e passado para o método
+        return service.createUser(user); // Retorna o usuário criado
+    }
 }
